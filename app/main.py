b3daa8e15e4f83661634f6087deda2bea1ef0860
Jaanvi Mutreja <jaanvimutreja90@gmail.com>
2026-08-23 16:15:55 +0530

    Allow portfolio development origin

--- app/main.py
+++ app/main.py
@@ -87,6 +87,7 @@ app.add_middleware(
     allow_origins=[
         "https://jaanviportfolio.vercel.app",
         "http://localhost:3000",
+        "http://localhost:3001",
     ],
     allow_credentials=True,
     allow_methods=["*"],
